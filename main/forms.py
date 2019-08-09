20442bf7f1ba8516e78d0b09749137caca5ba5fd
Cpsony <cpsony96@gmail.com>
2019-08-09 10:58:57 +0800

    添加新建项目Form

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from django import forms
+
+
 class UserForm(forms.Form):
-    username = forms.CharField(label='用户名',max_length=100)
-    password = forms.CharField(label='密_码',widget=forms.PasswordInput())
+    username = forms.CharField(label='用户名', max_length=100)
+    password = forms.CharField(label='密_码', widget=forms.PasswordInput())
     password2 = forms.CharField(label='密_码2', widget=forms.PasswordInput())
+
+
+class projectForm(forms.Form):
+    projectName = forms.CharField(max_length=50, label='项目名称')
+    moduleName = forms.CharField(max_length=50, label='模块名称')
