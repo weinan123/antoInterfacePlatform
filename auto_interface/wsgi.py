427e22b04f1b2068073ab307bf08f6e860c9e2f0
weinan123 <2767055703@qq.com>
2019-09-10 15:26:29 +0800

    wsgi

--- auto_interface/wsgi.py
+++ auto_interface/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
+from os.path import join, dirname, abspath
+PROJECT_DIR = dirname(dirname(abspath(__file__)))  # 3
+import sys  # 4
+sys.path.insert(0, PROJECT_DIR)
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "auto_interface.settings")
 
 application = get_wsgi_application()
+
+
+
