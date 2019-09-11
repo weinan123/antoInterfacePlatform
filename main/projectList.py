212054a7c81fe9548bb181d0bb376c466457193c
Cpsony <cpsony96@gmail.com>
2019-09-11 19:59:16 +0800

    初步实现对于上传文件的正则匹配，具体需要等ruru回来

--- main/projectList.py
+++ main/projectList.py
@@ -7,6 +7,7 @@ from forms import UserForm, projectForm
 from django.contrib import auth
 from django.contrib.auth.models import User
 from django import forms
+import re
 
 
 def addProjectList(request):
@@ -23,10 +24,12 @@ def addProjectList(request):
                                                  host=form.cleaned_data['host'])
 
             counttable = countCase.objects.create(projectName=form.cleaned_data['projectName'],
-                                                  moduleName=form.cleaned_data['moduleName'],)
+                                                  moduleName=form.cleaned_data['moduleName'], )
             counttable.save()
             inter.save()
             return HttpResponseRedirect('/projectList/')
+
+
 def projectListInfo(request):
     resp = interfaceList.objects.values("id", "projectName", "host", "moduleName", "updateTime", "createTime")
     respList = list(resp)
@@ -91,9 +94,40 @@ def projectSort(request):
 
 
 def projectImport(request):
+    Flag = Flag2 = True
     if request.method == 'POST':
         f = request.FILES['file']
-        with open('main/upload/file.txt', 'wb+') as destination:
-            for chunk in f.chunks():
-                destination.write(chunk)
+        for line in f.readlines():  # 依次读取每行
+            line = line.strip()  # 去掉每行头尾空白
+            if not len(line) or line.startswith('#'):  # 判断是否是空行或注释行
+                continue  # 跳过
+            matchObj = re.match(r'(GET|POST) (https|http)://(.*?)/(.*) ', line, re.M | re.I)
+            if matchObj:
+                if (Flag):
+                    Flag = False
+                    method = matchObj.group(1)
+                    protocol = matchObj.group(2)
+                    host = matchObj.group(2) + r'://' + matchObj.group(3)
+                    url = r'/' + matchObj.group(4)
+                    print "method : ", method
+                    print "protocol : ", protocol
+                    print "host : ", host
+                    print "url : ", url
+            matchObj2 = re.match(r'Content-Type: (.*?);(.*)', line, re.M | re.I)
+            if matchObj2:
+                if (Flag2):
+                    Flag2 = False
+                    header = '{"Content-Type": "' + matchObj2.group(1) + '"}'  # {"Content-Type": "application/json"}
+                    print "header : ", header
+            matchObj3 = re.finditer(r'".+?":".+?"', line, re.M | re.I)
+            if matchObj3:
+                for match in matchObj3:
+                    print (match.group())
+                    json = re.finditer(r'".+?"', match.group())
+                    for jsonMatch in json:
+                        print jsonMatch.group()
+
+        # with open('main/upload/file.txt', 'wb+') as destination:
+        #     for chunk in f.chunks():
+        #         destination.write(chunk)
     return HttpResponseRedirect('/projectList/')
