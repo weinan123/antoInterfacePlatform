63e9cda3e00f3bd718a7fa39b978a08464f84adb
yangruru2019 <153137558@qq.com>
2019-11-27 15:02:25 +0800

    修改批量删除bug

--- main/common/batchUntils.py
+++ main/common/batchUntils.py
@@ -223,7 +223,7 @@ def replaceStrParam(dependdata_dict, stringValue):
         else:
             strValue = strValue.replace("${"+param_key+"}", "")
             print(u"使用值：''替换参数${%s}" % param_key)
-        strValue = replaceParam(dependdata_dict, strValue)
+        strValue = replaceStrParam(dependdata_dict, strValue)
     else:
         return strValue
     return strValue
